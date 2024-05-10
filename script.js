6a321fb3a099cefbec2a430450405b495166e7b1
Andre Barros <andrebarros7201@gmail.com>
2024-05-10 17:25:20 +0100

    display score

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const Player = (name, ticker) => {
   const getName = () => name;
   const getTicker = () => ticker;
-  return { getName, getTicker };
+  let score = 0;
+  const getScore = () => score;
+  const incrementScore = () => score++;
+
+  return { getName, getTicker, getScore, incrementScore };
 };
 
 const GameBoard = () => {
-  //table[row][col]
-  let table = [
-    [[], [], []],
-    [[], [], []],
-    [[], [], []],
-  ];
 
   const createBoard = () => {
     const container = document.querySelector(".container");
@@ -48,14 +46,15 @@ const GameBoard = () => {
     }
   };
 
-  const declareWinner = (winnerName) => {
-    alert(`Winner is ${winnerName}`);
+  const declareWinner = (player) => {
+    alert(`Winner is ${player.getName()}`);
   };
 
-  const checkLine = (square1, square2, square3, playerName) => {
+  const checkLine = (square1, square2, square3, player) => {
     if (square1.isEqualNode(square2) && square2.isEqualNode(square3)) {
       console.log("winner");
-      declareWinner(playerName);
+      declareWinner(player);
+      player.incrementScore();
       reset();
       return true;
     } else {
@@ -63,31 +62,32 @@ const GameBoard = () => {
     }
   };
 
-  const checkWinner = (playerName) => {
+  const checkWinner = (player) => {
+
     const [s1, s2, s3, s4, s5, s6, s7, s8, s9] = getSquares();
     if (s1.hasChildNodes() && s2.hasChildNodes() && s3.hasChildNodes()) {
-      checkLine(s1.firstChild, s2.firstChild, s3.firstChild, playerName);
+      checkLine(s1.firstChild, s2.firstChild, s3.firstChild, player);
     }
     if (s4.hasChildNodes() && s5.hasChildNodes() && s6.hasChildNodes()) {
-      checkLine(s4.firstChild, s5.firstChild, s6.firstChild, playerName);
+      checkLine(s4.firstChild, s5.firstChild, s6.firstChild, player);
     }
     if (s7.hasChildNodes() && s8.hasChildNodes() && s9.hasChildNodes()) {
-      checkLine(s7.firstChild, s8.firstChild, s9.firstChild, playerName);
+      checkLine(s7.firstChild, s8.firstChild, s9.firstChild, player);
     }
     if (s1.hasChildNodes() && s4.hasChildNodes() && s7.hasChildNodes()) {
-      checkLine(s1.firstChild, s4.firstChild, s7.firstChild, playerName);
+      checkLine(s1.firstChild, s4.firstChild, s7.firstChild, player);
     }
     if (s2.hasChildNodes() && s5.hasChildNodes() && s8.hasChildNodes()) {
-      checkLine(s2.firstChild, s5.firstChild, s8.firstChild, playerName);
+      checkLine(s2.firstChild, s5.firstChild, s8.firstChild, player);
     }
     if (s3.hasChildNodes() && s6.hasChildNodes() && s9.hasChildNodes()) {
-      checkLine(s3.firstChild, s6.firstChild, s9.firstChild, playerName);
+      checkLine(s3.firstChild, s6.firstChild, s9.firstChild, player);
     }
     if (s1.hasChildNodes() && s5.hasChildNodes() && s9.hasChildNodes()) {
-      checkLine(s1.firstChild, s5.firstChild, s9.firstChild, playerName);
+      checkLine(s1.firstChild, s5.firstChild, s9.firstChild, player);
     }
     if (s7.hasChildNodes() && s5.hasChildNodes() && s3.hasChildNodes()) {
-      checkLine(s7.firstChild, s5.firstChild, s3.firstChild, playerName);
+      checkLine(s7.firstChild, s5.firstChild, s3.firstChild, player);
     }
   };
 
@@ -115,9 +115,15 @@ const Game = () => {
   const p1 = Player("John", "X");
   const p2 = Player("Michael", "O");
 
+  const displayScore = () =>{
+    const score = document.querySelector('#playerScore');
+    score.textContent = `${p1.getScore()} - ${p2.getScore()}`;
+  }
+
+
   const left = document.querySelector("#left");
   const right = document.querySelector("#right");
-  
+
   if (!left.hasChildNodes() && !right.hasChildNodes()) {
     const nameP1 = document.createElement("h2");
     nameP1.textContent = p1.getName();
@@ -145,8 +151,9 @@ const Game = () => {
       if (gameBoard.checkSquareValue(square)) {
         gameBoard.setTicker(square, lastPlayer.getTicker());
         togglePlayer();
-        gameBoard.checkWinner(lastPlayer.getName());
+        gameBoard.checkWinner(lastPlayer);
         console.log(square.id, lastPlayer.getTicker());
+        displayScore();
       }
     });
   });
